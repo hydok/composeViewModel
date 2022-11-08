af32f5c55534370ede421e94c75b133efe5ebe18
hydok <hydok@naver.com>
2022-11-08 13:47:48 +0900

    Update MainActivity.kt

--- app/src/main/java/io/hydok/composeviewmodel/MainActivity.kt
+++ app/src/main/java/io/hydok/composeviewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package io.hydok.composeviewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,31 +14,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import io.hydok.composeviewmodel.ui.theme.ComposeViewModelTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeViewModelTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainApp(viewModel = mainViewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeViewModelTheme {
-        Greeting("Android")
     }
 }
